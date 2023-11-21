0ec4b609d0f45cb23e3139ec6d062c5e5c04e182
igorfelips <igorfelipe844@gmail.com>
2023-11-21 10:46:23 -0300

    First config

---
+++ src/main/java/com/projeto/concessionaria/ProjetoConcessionariaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.concessionaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoConcessionariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoConcessionariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/concessionaria/ProjetoConcessionariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.concessionaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoConcessionariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
