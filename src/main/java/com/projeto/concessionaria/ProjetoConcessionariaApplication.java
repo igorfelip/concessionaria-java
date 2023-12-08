64a540fb6f5b7c391cdce62f080d7888ad5d9d6f
igorfelips <igorfelipe844@gmail.com>
2023-12-07 22:24:16 -0300

    feat: config

--- src/main/java/com/projeto/concessionaria/ProjetoConcessionariaApplication.java
+++ src/main/java/com/projeto/concessionaria/ProjetoConcessionariaApplication.java
@@ -2,8 +2,10 @@ package com.projeto.concessionaria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ProjetoConcessionariaApplication {
 
 	public static void main(String[] args) {
