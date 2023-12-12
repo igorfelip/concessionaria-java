afbc040089069c9cb21ddf2a062f713b2fef92e4
IGOR FELIPE SILVA DE OLIVEIRA <igorfelipe844@gmail.com>
2023-12-12 13:05:45 -0300

    feat: Utils class

--- src/main/java/com/projeto/concessionaria/utils/Utils.java
+++ src/main/java/com/projeto/concessionaria/utils/Utils.java
@@ -7,6 +7,8 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class Utils {
+    private Utils() {
+    }
 
     public static BigDecimal calculaComissao(Vendedores vendedores, Carros carros) {
         BigDecimal salario = vendedores.getSalario();
@@ -24,47 +26,47 @@ public class Utils {
             case 2:
                 valorParcial = preco.multiply(BigDecimal.valueOf(0.02));
                 valorParcela = (preco.add(valorParcial));
-                return valorParcela.divide(BigDecimal.valueOf(2).setScale(2, RoundingMode.HALF_EVEN));
+                return valorParcela.divide(BigDecimal.valueOf(2).setScale(2, RoundingMode.HALF_EVEN), RoundingMode.HALF_EVEN);
             case 3:
                 valorParcial = preco.multiply(BigDecimal.valueOf(0.04));
                 valorParcela = (preco.add(valorParcial));
-                return valorParcela.divide(BigDecimal.valueOf(3).setScale(2, RoundingMode.HALF_EVEN));
+                return valorParcela.divide(BigDecimal.valueOf(3).setScale(2, RoundingMode.HALF_EVEN), RoundingMode.HALF_EVEN);
             case 4:
                 valorParcial = preco.multiply(BigDecimal.valueOf(0.06));
                 valorParcela = (preco.add(valorParcial));
-                return valorParcela.divide(BigDecimal.valueOf(4).setScale(2, RoundingMode.HALF_EVEN));
+                return valorParcela.divide(BigDecimal.valueOf(4).setScale(2, RoundingMode.HALF_EVEN), RoundingMode.HALF_EVEN);
             case 5:
                 valorParcial = preco.multiply(BigDecimal.valueOf(0.08));
                 valorParcela = (preco.add(valorParcial));
-                return valorParcela.divide(BigDecimal.valueOf(5).setScale(2, RoundingMode.HALF_EVEN));
+                return valorParcela.divide(BigDecimal.valueOf(5).setScale(2, RoundingMode.HALF_EVEN), RoundingMode.HALF_EVEN);
             case 6:
                 valorParcial = preco.multiply(BigDecimal.valueOf(0.10));
                 valorParcela = (preco.add(valorParcial));
-                return valorParcela.divide(BigDecimal.valueOf(6).setScale(2, RoundingMode.HALF_EVEN));
+                return valorParcela.divide(BigDecimal.valueOf(6).setScale(2, RoundingMode.HALF_EVEN), RoundingMode.HALF_EVEN);
             case 7:
                 valorParcial = preco.multiply(BigDecimal.valueOf(0.12));
                 valorParcela = (preco.add(valorParcial));
-                return valorParcela.divide(BigDecimal.valueOf(7).setScale(2, RoundingMode.HALF_EVEN));
+                return valorParcela.divide(BigDecimal.valueOf(7).setScale(2, RoundingMode.HALF_EVEN), RoundingMode.HALF_EVEN);
             case 8:
                 valorParcial = preco.multiply(BigDecimal.valueOf(0.14));
                 valorParcela = (preco.add(valorParcial));
-                return valorParcela.divide(BigDecimal.valueOf(8).setScale(2, RoundingMode.HALF_EVEN));
+                return valorParcela.divide(BigDecimal.valueOf(8).setScale(2, RoundingMode.HALF_EVEN), RoundingMode.HALF_EVEN);
             case 9:
                 valorParcial = preco.multiply(BigDecimal.valueOf(0.16));
                 valorParcela = (preco.add(valorParcial));
-                return valorParcela.divide(BigDecimal.valueOf(9).setScale(2, RoundingMode.HALF_EVEN));
+                return valorParcela.divide(BigDecimal.valueOf(9).setScale(2, RoundingMode.HALF_EVEN), RoundingMode.HALF_EVEN);
             case 10:
                 valorParcial = preco.multiply(BigDecimal.valueOf(0.18));
                 valorParcela = (preco.add(valorParcial));
-                return valorParcela.divide(BigDecimal.valueOf(10).setScale(2, RoundingMode.HALF_EVEN));
+                return valorParcela.divide(BigDecimal.valueOf(10).setScale(2, RoundingMode.HALF_EVEN), RoundingMode.HALF_EVEN);
             case 11:
                 valorParcial = preco.multiply(BigDecimal.valueOf(0.20));
                 valorParcela = (preco.add(valorParcial));
-                return valorParcela.divide(BigDecimal.valueOf(11).setScale(2, RoundingMode.HALF_EVEN));
+                return valorParcela.divide(BigDecimal.valueOf(11).setScale(2, RoundingMode.HALF_EVEN), RoundingMode.HALF_EVEN);
             case 12:
                 valorParcial = preco.multiply(BigDecimal.valueOf(0.22));
                 valorParcela = (preco.add(valorParcial));
-                return valorParcela.divide(BigDecimal.valueOf(12).setScale(2, RoundingMode.HALF_EVEN));
+                return valorParcela.divide(BigDecimal.valueOf(12).setScale(2, RoundingMode.HALF_EVEN), RoundingMode.HALF_EVEN);
             default:
                 return BigDecimal.valueOf(0.0);
 
